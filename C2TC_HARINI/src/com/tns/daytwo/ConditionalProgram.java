59e148b1551033ef84902305143139b4e11107fa
striverCoder-Santhosh <santhosh93453@gmail.com>
2024-09-04 15:39:08 +0530

    This is my Third commit in Second Program

---
+++ C2TC_HARINI/src/com/tns/daytwo/ConditionalProgram.java
@@ -0,0 +1,20 @@
+package com.tns.daytwo;
+
+public class ConditionalProgram {
+	
+	    public static void main(String[] args) {
+	        // Example variables
+	        int number = 15;
+
+	        // Using if-else statements to check conditions
+	        if (number > 0) {
+	            System.out.println("The number is positive.");
+	        } else if (number < 0) {
+	            System.out.println("The number is negative.");
+	        } else {
+	            System.out.println("The number is zero.");
+	        }
+	    }
+	}
+
+
